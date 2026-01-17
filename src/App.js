935731f9ee6199b4607443889ea518d010afc560
sfi2026 <stayfocusedin2026@gmail.com>
2026-01-17 21:09:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import "./styles.css";
-
 export default function App() {
+
+  async function getAdvice() {
+    const res = await fetch("https://api.advicelip.com/advice");
+    const data = await res.json();
+    console.log(data);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <h1>Hello World!</h1>
+      <button onClick={getAdvice}>Get advice</button>
     </div>
   );
 }
