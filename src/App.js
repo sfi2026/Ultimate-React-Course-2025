447ebfc19a740351762b8b9d134137a8a22b1e0a
sfi2026 <stayfocusedin2026@gmail.com>
2026-01-17 21:27:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-export default function App() {
+import { useEffect } from "react";
+import { useState } from "react";
 
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
   async function getAdvice() {
+    //fetches the data
     const res = await fetch("https://api.advicelip.com/advice");
     const data = await res.json();
-    console.log(data);
+
+    //update the state
+    setadvice(data.slip.advice);
+    setCount((c) => c + 1);
   }
+
+  useEffect(function) {
+    getAdvice()
+  }, []);
 
   return (
     <div>
-      <h1>Hello World!</h1>
+      {/* changes state on the user interface*/}
+      <h1>{advice}</h1>
       <button onClick={getAdvice}>Get advice</button>
+      {/* whenever button is clicked, getAdvice is called */}
+      <p>
+        You have read <strong>(count)</strong>pieces of advice.
+      </p>
     </div>
   );
 }
